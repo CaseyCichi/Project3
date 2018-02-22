9d6a3a03f1643efc567f5d03f3933e24c07c1c13
Dismes <yuhki.n@gmail.com>
2018-02-21 16:42:52 -0800

    added calendar and some npm stuff

--- server/db/controllers/googleCalendarEvents.js
+++ server/db/controllers/googleCalendarEvents.js
@@ -1,2 +1,37 @@
 import Events from '../models/googleCalendarEvent';
 
+var googleAuth = require("./googleauthorize.js");
+var Calendarfunction = require("./googleCalendarFunctions");
+var TOKEN_PATH = "../../config/googleSecrets/calendar-node-quickstart.json";
+
+
+function listevents(NameOfCalendar) {
+    googleAuth.returnOath(TOKEN_PATH, function (auth) {
+      Calendarfunction.listEvents(auth, NameOfCalendar)
+    })
+  }
+  
+  function createEventInCalendar(CalendarID, Events) {
+    googleAuth.returnOath(TOKEN_PATH, function (auth) {
+      Calendarfunction.createEvent(auth, CalendarID, Events);
+    })
+  }
+  
+  function createCalendar(NameOfCalendar) {
+    googleAuth.returnOath(TOKEN_PATH, function (auth) {
+      Calendarfunction.postCreateCalendar(auth, NameOfCalendar);
+    })
+  }
+  
+  function deleteCalendar(idOfCalendar){
+    googleAuth.returnOath(TOKEN_PATH, function(auth){
+      Calendarfunction.deleteCalendar(auth, idOfCalendar);
+    })
+  }
+  
+  module.exports = {
+    listevents,
+    createEventInCalendar,
+    deleteCalendar,
+    createCalendar,
+  }
